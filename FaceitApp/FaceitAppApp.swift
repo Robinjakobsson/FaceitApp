1886df296132474285aa80df667c5610d2455308
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-05-31 12:03:05 +0200

    Initial Commit

---
+++ FaceitApp/FaceitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceitAppApp.swift
+//  FaceitApp
+//
+//  Created by Robin jakobsson on 2025-05-31.
+//
+
+import SwiftUI
+
+@main
+struct FaceitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FaceitAppTests/FaceitAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceitAppTests.swift
+//  FaceitAppTests
+//
+//  Created by Robin jakobsson on 2025-05-31.
+//
+
+import Testing
+@testable import FaceitApp
+
+struct FaceitAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
